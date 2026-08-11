62081ecd4b5ba8260802509c1b910a2c8a268a37
harendrapandit03 <6206.harendra@gmail.com>
2026-08-11 22:00:30 +0530

    modified models

--- backend/models.py
+++ backend/models.py
@@ -2,10 +2,11 @@ from .database import Base
 from sqlalchemy import Column,Integer ,String
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import Mapped ,mapped_column;
 
 class User(Base):
     __tablename__="users"
-    id=Column(Integer,primary_key=True,nullable=False)
+    id:Mapped[str]=mapped_column(Integer,primary_key=True,nullable=False)
     name=Column(String,nullable=False)
     age=Column(Integer,nullable=False)
     email=Column(String,nullable=False)
