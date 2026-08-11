c9523f165c7b5937a97f6302c2d93ce777b9927e
harendrapandit03 <6206.harendra@gmail.com>
2026-08-11 22:10:29 +0530

    models -- modified

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,13 @@
 from .database import Base
-from sqlalchemy import Column,Integer ,String
-from sqlalchemy.sql.expression import text
+from sqlalchemy import Integer ,String
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import Mapped ,mapped_column;
 
 class User(Base):
     __tablename__="users"
-    id:Mapped[str]=mapped_column(Integer,primary_key=True,nullable=False)
-    name=Column(String,nullable=False)
-    age=Column(Integer,nullable=False)
-    email=Column(String,nullable=False)
-    password=Column(String,nullable=False)
-    created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=('now()'))
+    id:Mapped[int]=mapped_column(Integer,primary_key=True,nullable=False)
+    name:Mapped[str]=mapped_column(String,nullable=False)
+    age:Mapped[int]=mapped_column(Integer,nullable=False)
+    email:Mapped[str]=mapped_column(String,nullable=False)
+    password:Mapped[str]=mapped_column(String,nullable=False)
+    created_at:Mapped[TIMESTAMP]=mapped_column(TIMESTAMP(timezone=True),nullable=False,server_default=('now()'))
